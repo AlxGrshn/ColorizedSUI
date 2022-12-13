0871211cf26f4997a8c130ee0e2c445854fc232f
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-12-13 16:24:10 +0300

    Initial Commit

---
+++ ColorizedSUI/ColorizedSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSUIApp.swift
+//  ColorizedSUI
+//
+//  Created by Alexander Grishin on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
